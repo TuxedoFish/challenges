7f79959327468b099114fa519e6300ede8ee4927
Harry Liversedge <harryliversedge@gmail.com>
2021-06-13 18:08:26 +0100

    add delete duplicates solution

--- src/main/java/com/tuxedofish/challenges/linkedlist/LinkedListProblems.java
+++ src/main/java/com/tuxedofish/challenges/linkedlist/LinkedListProblems.java
@@ -1,7 +1,25 @@
 package com.tuxedofish.challenges.linkedlist;
 
 public class LinkedListProblems {
+	/* 83. Remove duplicates from sorted linked list
+	   Given the head of a sorted linked list, 
+	   delete all duplicates such that each 
+	   element appears only once. 
+	   Return the linked list sorted as well.
+    */
     public static ListNode deleteDuplicates(ListNode head) {
+    	if(head == null) { return null; }
+    	ListNode current = head;
+    	
+    	while(current.next != null) {
+    		// As sorted if the same value can remove
+    		if(current.next.val == current.val) {
+    			current.next = current.next.next;
+    		} else {
+    			current = current.next;
+    		}
+    	}
+    	
     	return head;
     }
 }
--- src/main/java/com/tuxedofish/challenges/linkedlist/ListNode.java
+++ src/main/java/com/tuxedofish/challenges/linkedlist/ListNode.java
@@ -7,6 +7,8 @@ public class ListNode {
 	ListNode() {}
     ListNode(int val) { this.val = val; }
     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    
+    @Override
     public String toString(){  
     	StringBuilder list = new StringBuilder();
     	list.append('[');
@@ -20,4 +22,31 @@ public class ListNode {
     	list.append(']');
     	return list.toString();
 	}  
+    
+    @Override
+    public boolean equals(Object o) {
+    	// If the object is compared with itself then return true  
+        if (o == this) {
+            return true;
+        }
+  
+        // Check if the same instance
+        if (!(o instanceof ListNode)) {
+            return false;
+        }
+          
+        // Type cast o to ListNode 
+        ListNode c = (ListNode) o;
+        ListNode curr = this;
+        
+        // Compare the data members and return accordingly 
+        while(c != null) {
+        	if(curr == null) { return false; }
+            if(c.val != curr.val) { return false; }
+            
+            curr = curr.next;
+            c = c.next;
+        }
+        return true;
+    }
 }
