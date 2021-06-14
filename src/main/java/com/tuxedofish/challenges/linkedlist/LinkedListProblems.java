bcbacb5a23395d764c4220411854e9635a3cad94
Harry Liversedge <harryliversedge@gmail.com>
2021-06-14 19:30:44 +0100

    add slow solution

--- src/main/java/com/tuxedofish/challenges/linkedlist/LinkedListProblems.java
+++ src/main/java/com/tuxedofish/challenges/linkedlist/LinkedListProblems.java
@@ -22,4 +22,31 @@ public class LinkedListProblems {
     	
     	return head;
     }
+    /* 
+     * 160. Intersection of Two Linked Lists
+     * Given the heads of two singly linked-lists 
+     * headA and headB, return the node at which 
+     * the two lists intersect. If the two linked 
+     * lists have no intersection at all, return null.
+     */
+    public static ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if(headA == null || headB == null) { return null; }
+    	
+    	ListNode currA = headA;
+    	
+    	while(currA != null) {
+        	ListNode currB = headB;
+        	
+    		while(currB != null) {
+    			if(currB == currA) {
+                    return currA;
+    			}
+    			currB = currB.next;
+    		}
+    		
+    		currA = currA.next;
+    	}
+    	
+    	return null;
+    }
 }
---
+++ src/test/java/com/tuxedofish/challenges/linkedlist/IntersectionTest.java
@@ -0,0 +1,30 @@
+package com.tuxedofish.challenges.linkedlist;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class IntersectionTest {
+    @Test                                               
+    @DisplayName("Returns correct node of intersection")   
+    public void simpleCase() {
+    	ListNode expected = LinkedListUtils.generateLinkedListFromArray(new int[] {8, 4, 5});	
+    	
+        ListNode inputA = LinkedListUtils.generateLinkedListFromArray(new int[] {4, 1});
+        inputA.next = expected;
+        ListNode inputB = LinkedListUtils.generateLinkedListFromArray(new int[] {5, 6, 1});
+        inputB.next = expected;
+       
+        ListNode actual = LinkedListProblems.getIntersectionNode(inputA, inputB);
+        assertEquals(expected, actual);
+    } 
+    @Test                                               
+    @DisplayName("Returns null if input is null")   
+    public void nullCase() {
+        ListNode inputA = null;
+        ListNode inputB = LinkedListUtils.generateLinkedListFromArray(new int[] {5, 6, 1, 8, 4, 5});
+        ListNode expected = null;
+        ListNode actual = LinkedListProblems.getIntersectionNode(inputA, inputB);
+        assertEquals(expected, actual);
+    }
+}
