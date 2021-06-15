2ccfa637bb600856c8c4d0664da706fd90521c78
Harry Liversedge <harryliversedge@gmail.com>
2021-06-15 11:51:40 +0100

    add explanation of complexity

--- src/main/java/com/tuxedofish/challenges/binarytree/BinarySearchProblems.java
+++ src/main/java/com/tuxedofish/challenges/binarytree/BinarySearchProblems.java
@@ -6,6 +6,13 @@ public class BinarySearchProblems {
     }
     
     public static int searchRecursive(int[] nums, int target, int left, int right) {
+    	// Binary search is O(log(n))       
+    	// This is because in the worst case
+    	// it takes k iterations to get to 1
+    	// Worst case is N/(2^k) = 1
+    	// N = 2^k
+    	// k = log(n)
+    	// Space complexity is O(1) constant space
         int mid = left + ((right - left) / 2);
         int curr = nums[mid];
         
