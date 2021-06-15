81d599dce456674419e97e165ace96da17e2346a
Harry Liversedge <harryliversedge@gmail.com>
2021-06-15 11:22:09 +0100

    add tests for binary search

---
+++ src/test/java/com/tuxedofish/challenges/binarytree/BinarySearchTest.java
@@ -0,0 +1,59 @@
+package com.tuxedofish.challenges.binarytree;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class BinarySearchTest {
+	@Test                                               
+    @DisplayName("Returns true with even input")   
+    public void evenLengthCase() {
+    	int expected = 4;
+    	int[] input = new int[] {-1,0,3,5,9,12};
+    	int target = 9;
+    	
+    	int actual = BinarySearchProblems.search(input, target);
+        assertEquals(expected, actual);
+    }  
+	@Test                                               
+    @DisplayName("Returns true with even input")   
+    public void oddLengthCase() {
+    	int expected = 1;
+    	int[] input = new int[] {-5,3,6,12,19};
+    	int target = 3;
+    	
+    	int actual = BinarySearchProblems.search(input, target);
+        assertEquals(expected, actual);
+    }
+	@Test                                               
+    @DisplayName("Returns true with target at end")   
+    public void endCase() {
+    	int expected = 5;
+    	int[] input = new int[] {-1,7,6,5,12,19};
+    	int target = 19;
+    	
+    	int actual = BinarySearchProblems.search(input, target);
+        assertEquals(expected, actual);
+    }  
+	@Test                                               
+    @DisplayName("Returns true with target at end")   
+    public void startCase() {
+    	int expected = 0;
+    	int[] input = new int[] {-6,0,3,5,12};
+    	int target = -6;
+    	
+    	int actual = BinarySearchProblems.search(input, target);
+        assertEquals(expected, actual);
+    }  
+	@Test                                               
+    @DisplayName("Returns true with target as -1")   
+    public void edgeCase() {
+    	int expected = 0;
+    	int[] input = new int[] {-1,0,2,3,4,5};
+    	int target = -1;
+    	
+    	int actual = BinarySearchProblems.search(input, target);
+        assertEquals(expected, actual);
+    }  
+}
