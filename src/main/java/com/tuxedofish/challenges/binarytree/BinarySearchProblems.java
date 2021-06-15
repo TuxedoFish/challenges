5de35c212ca81f0d3fbd185cae20d7c7136225c5
Harry Liversedge <harryliversedge@gmail.com>
2021-06-15 11:27:46 +0100

    do not run tests until built

--- src/main/java/com/tuxedofish/challenges/binarytree/BinarySearchProblems.java
+++ src/main/java/com/tuxedofish/challenges/binarytree/BinarySearchProblems.java
@@ -2,6 +2,23 @@ package com.tuxedofish.challenges.binarytree;
 
 public class BinarySearchProblems {
 	public static int search(int[] nums, int target) {
-		return -1;
-	}
+        return searchRecursive(nums, target, 0, nums.length - 1);
+    }
+    
+    public static int searchRecursive(int[] nums, int target, int left, int right) {
+        int mid = left + ((right - left) / 2);
+        int curr = nums[mid];
+        
+        if(left == right) {
+            return curr == target ? mid : -1; 
+        }
+        
+        if(curr < target) {
+            return searchRecursive(nums, target, mid + 1, right);
+        } else if (curr > target) {
+            return searchRecursive(nums, target, left, mid - 1);
+        } else {
+            return mid;
+        }
+    }
 }
