9d031713d0ded3a6f4ba3af829c3fd5a6c0208cc
TuxedoFish <Harry.Liversedge@ibm.com>
2021-06-19 09:13:49 +0100

    add an extra test

--- src/test/java/com/tuxedofish/challenges/binarytree/findduplicateinarray/UnitTests.java
+++ src/test/java/com/tuxedofish/challenges/binarytree/findduplicateinarray/UnitTests.java
@@ -18,10 +18,17 @@ public class UnitTests {
     }
 	
 	@Test                                               
-    @DisplayName("Returns true that has node")   
+    @DisplayName("Returns duplicate for medium array")   
     public void simpleCase() {
     	int[] nums = new int[] {1,2,3,3};
         int actual = solution.findDuplicate(nums);
         assertEquals(3, actual);
+    }
+	@Test                                               
+    @DisplayName("Returns duplicate within small array")   
+    public void smallestArrayCase() {
+    	int[] nums = new int[] {1,1};
+        int actual = solution.findDuplicate(nums);
+        assertEquals(1, actual);
     }
 }
